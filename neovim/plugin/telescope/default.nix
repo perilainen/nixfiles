9ab11aab28d124c5e773acb8fcff8baacfd34ea6
Per Johansson <per.a.johansson@svt.se>
2025-04-01 09:10:03 +0200

    add telescope conf

---
+++ neovim/plugin/telescope/default.nix
@@ -0,0 +1,12 @@
+{
+  telescope = {
+    enable = true;
+    extensions.fzf-native.enable = true;
+    settings.defaults.layout_config.vertical.height = 0.5;
+    keymaps = {
+      "<leader>st" = "live_grep";
+      "<leader>sf" = "find_files";
+      "<leader>sb" = "buffers";
+    };
+  };
+}
